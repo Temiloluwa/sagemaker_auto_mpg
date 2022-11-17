3128582a9320103da562f95eb4da8c948861aa60
temiloluwa <temireply@gmail.com>
2022-11-17 20:47:06 +0100

    change from test to val in model.train

--- scripts/model/train.py
+++ scripts/model/train.py
@@ -16,22 +16,22 @@ stream_handler.setFormatter(logging.Formatter("%(asctime)s %(name)-12s %(levelna
 logger.addHandler(stream_handler)
 
 
-def get_metrics(train_y_true, train_y_pred, test_y_true, test_y_pred):
+def get_metrics(train_y_true, train_y_pred, val_y_true, val_y_pred):
     """
-    Return train and test metrics
+    Return train and val metrics
     """
     
     # mae
     t_mae = mean_absolute_error(train_y_true, train_y_pred)
-    ts_mae = mean_absolute_error(test_y_true, test_y_pred)
+    ts_mae = mean_absolute_error(val_y_true, val_y_pred)
     
     # mse
     t_mse = mean_squared_error(train_y_true, train_y_pred, squared=False)
-    ts_mse = mean_squared_error(test_y_true, test_y_pred, squared=False)
+    ts_mse = mean_squared_error(val_y_true, val_y_pred, squared=False)
     
     # rmse
     t_rmse = mean_squared_error(train_y_true, train_y_pred, squared=True)
-    ts_rmse = mean_squared_error(test_y_true, test_y_pred, squared=True)
+    ts_rmse = mean_squared_error(val_y_true, val_y_pred, squared=True)
     
     return t_mae, ts_mae, t_mse, ts_mse, t_rmse, ts_rmse
 
@@ -61,9 +61,9 @@ if __name__=='__main__':
     train_feats = train_data[:, 1:]
     train_target = train_data[:, 0]
     
-    test_data = np.load(os.path.join(args.test, "test_feats.npy"))
-    test_feats = test_data[:, 1:]
-    test_target = test_data[:, 0]
+    val_data = np.load(os.path.join(args.test, "val_feats.npy"))
+    val_feats = val_data[:, 1:]
+    val_target = val_data[:, 0]
     
     # Train random forest model
     model = RandomForestRegressor(max_depth=args.n_estimators, random_state=0)
@@ -71,15 +71,15 @@ if __name__=='__main__':
     logger.info("Model Trained ")
     
     train_pred = model.predict(train_feats)
-    test_pred = model.predict(test_feats)
+    val_pred = model.predict(val_feats)
     
     # Evaluate Model
-    train_mae, test_mae, train_mse, test_mse, train_rmse, test_rmse = \
-        get_metrics(train_target, train_pred, test_target, test_pred)
+    train_mae, val_mae, train_mse, val_mse, train_rmse, val_rmse = \
+        get_metrics(train_target, train_pred, val_target, val_pred)
     
-    logger.info(f"train_mae={train_mae};  test_mae={test_mae};")
-    logger.info(f"train_mse={train_mse};  test_mse={test_mse};")
-    logger.info(f"train_rmse={train_rmse}; test_rmse={test_rmse};")
+    logger.info(f"train_mae={train_mae};  val_mae={val_mae};")
+    logger.info(f"train_mse={train_mse};  val_mse={val_mse};")
+    logger.info(f"train_rmse={train_rmse}; val_rmse={val_rmse};")
     
     # Save the Model
     joblib.dump(model, os.path.join(args.model_dir, args.model_filename))
