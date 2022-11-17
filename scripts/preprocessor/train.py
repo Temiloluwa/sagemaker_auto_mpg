3c4bd8e85825c4904b71d764fff5334658900670
temiloluwa <temireply@gmail.com>
2022-11-17 20:44:16 +0100

    change from test to val in preprocessor.train

--- scripts/preprocessor/train.py
+++ scripts/preprocessor/train.py
@@ -17,17 +17,17 @@ def save_numpy(np_array, path):
 
 if __name__ == '__main__':
     CONTAINER_TRAIN_INPUT_PATH = "/opt/ml/processing/input/train"
-    CONTAINER_TEST_INPUT_PATH = "/opt/ml/processing/input/test"
+    CONTAINER_VAL_INPUT_PATH = "/opt/ml/processing/input/test"
     CONTAINER_TRAIN_OUTPUT_PATH = "/opt/ml/processing/train"
-    CONTAINER_TEST_OUTPUT_PATH = "/opt/ml/processing/test"
+    CONTAINER_VAL_OUTPUT_PATH = "/opt/ml/processing/test"
     CONTAINER_OUTPUT_PATH = "/opt/ml/processing/output"
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--train-filename', type=str, default='train.csv')
-    parser.add_argument('--test-filename', type=str, default='test.csv')
+    parser.add_argument('--val-filename', type=str, default='val.csv')
     parser.add_argument('--model-filename', type=str, default='model.tar.gz')
     parser.add_argument('--train-feats-filename', type=str, default='train_feats.npy')
-    parser.add_argument('--test-feats-filename', type=str, default='test_feats.npy')
+    parser.add_argument('--val-feats-filename', type=str, default='val_feats.npy')
     
     args = parser.parse_args()
 
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     pl = Pipeline([("custom-preprocessing", CustomFeaturePreprocessor()), ("column-preprocessing", ct)])
     
     train_data_path = os.path.join(CONTAINER_TRAIN_INPUT_PATH, args.train_filename)
-    test_data_path = os.path.join(CONTAINER_TEST_INPUT_PATH, args.test_filename)
+    val_data_path = os.path.join(CONTAINER_VAL_INPUT_PATH, args.val_filename)
     
     # preprocess features, first column is target and the rest are features
     train_df = pd.read_csv(train_data_path)
@@ -48,18 +48,18 @@ if __name__ == '__main__':
     train_target = train_df["mpg"].values.reshape(-1, 1)
     train_features = pl.fit_transform(train_data)
     
-    test_df = pd.read_csv(test_data_path)
-    test_data = test_df.iloc[:, 1:]
-    test_target = test_df["mpg"].values.reshape(-1, 1)
-    test_features = pl.transform(test_data)
+    val_df = pd.read_csv(val_data_path)
+    val_data = val_df.iloc[:, 1:]
+    val_target = val_df["mpg"].values.reshape(-1, 1)
+    val_features = pl.transform(val_data)
     
     # save features in output path with the container
     train_features = np.concatenate([train_target, train_features], axis=1)
-    test_features = np.concatenate([test_target, test_features], axis=1)
+    val_features = np.concatenate([val_target, val_features], axis=1)
     train_features_save_path = os.path.join(CONTAINER_TRAIN_OUTPUT_PATH, args.train_feats_filename)
-    test_features_save_path = os.path.join(CONTAINER_TEST_OUTPUT_PATH, args.test_feats_filename)
+    val_features_save_path = os.path.join(CONTAINER_VAL_OUTPUT_PATH, args.val_feats_filename)
     save_numpy(train_features, train_features_save_path)
-    save_numpy(test_features, test_features_save_path)
+    save_numpy(val_features, val_features_save_path)
     
     # save preprocessor model
     model_save_path = os.path.join(CONTAINER_OUTPUT_PATH, args.model_filename)
